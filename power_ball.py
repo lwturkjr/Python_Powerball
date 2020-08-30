014060a7ff66d2906c19ccd27f6e8be484a387d9
lwturkjr <lwturkjr@SgtSegFault.home>
2020-08-30 18:27:37 -0400

    Think it's done for now

--- power_ball.py
+++ power_ball.py
@@ -1,5 +1,5 @@
-"""
 #!/usr/bin/env python
+"""
 #==============================================================================================
 # Powerball Frequency analyzer and quick pick
 # basically a python re-write of
